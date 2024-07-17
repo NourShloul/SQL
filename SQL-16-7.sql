f3261fe52d0bb2b4a61671db7360d3da1761a888
NourShloul <shloulnour31@gmail.com>
2024-07-17 12:53:34 +0300

    OCA sql tasks

--- SQL-16-7.sql
+++ SQL-16-7.sql
@@ -1,59 +1,46 @@
-<<<<<<< HEAD
 CREATE DATABASE School;
 USE School;
 
-=======
-CREATE DATABASE School;
-USE School;
-
->>>>>>> 691914f287b5a213fb8212911a0cc551d263796a
-CREATE TABLE Teachers (
-    TeacherID INT PRIMARY KEY,
+CREATE TABLE Teacherss (
+    TeacherID INT PRIMARY KEY identity(1,1),
     FirstName VARCHAR(10),
     LastName VARCHAR(10)
-<<<<<<< HEAD
 );
 
-=======
-);
-
->>>>>>> 691914f287b5a213fb8212911a0cc551d263796a
-CREATE TABLE TeacherDetails (
-    DetailID INT PRIMARY KEY,
+CREATE TABLE TeacherDetailss (
+    DetailID INT PRIMARY KEY identity(1,1),
     TeacherID INT,
     Address VARCHAR(50),
     Phone VARCHAR(13),
     Email VARCHAR(50),
-    FOREIGN KEY (TeacherID) REFERENCES Teachers(TeacherID)
-<<<<<<< HEAD
+    FOREIGN KEY (TeacherID) REFERENCES Teacherss(TeacherID) ON DELETE CASCADE
 );
 
-=======
-);
-
->>>>>>> 691914f287b5a213fb8212911a0cc551d263796a
-CREATE TABLE Courses (
-    CourseID INT PRIMARY KEY,
+CREATE TABLE Coursess (
+    CourseID INT PRIMARY KEY ,
     CourseName VARCHAR(20),
     TeacherID INT,
-    FOREIGN KEY (TeacherID) REFERENCES Teachers(TeacherID)
-<<<<<<< HEAD
+    FOREIGN KEY (TeacherID) REFERENCES Teacherss(TeacherID) ON DELETE CASCADE
 );
 
-=======
-);
-
->>>>>>> 691914f287b5a213fb8212911a0cc551d263796a
-CREATE TABLE Students (
+CREATE TABLE Studentss (
     StudentID INT PRIMARY KEY,
     FirstName VARCHAR(10),
     LastName VARCHAR(10),
-    EnrolledCourseID INT,
-    FOREIGN KEY (EnrolledCourseID) REFERENCES Courses(CourseID)
-<<<<<<< HEAD
+    --EnrolledCourseID INT,
+    --FOREIGN KEY (EnrolledCourseID) REFERENCES Courses(CourseID) 
 );
 
-INSERT INTO Teachers VALUES
+CREATE TABLE Enrollments (
+    EnrollmentID INT PRIMARY KEY IDENTITY(1,1),
+    StudentID INT, -- Foreign key 
+    CourseID INT,  -- Foreign key 
+    EnrollmentDate DATE,
+    FOREIGN KEY (StudentID) REFERENCES Studentss(StudentID) ON DELETE CASCADE,
+    FOREIGN KEY (CourseID) REFERENCES Coursess(CourseID) ON DELETE CASCADE
+);
+
+INSERT INTO Teacherss VALUES
 (1,'Ayman', 'Jaradat'),
 (2,'Batool', 'Aldalki'),
 (3,'Amer', 'Abualhaija'),
@@ -61,67 +48,29 @@ INSERT INTO Teachers VALUES
 (5,'Mohammed', 'Frehat');
 
 
+INSERT INTO TeacherDetailss VALUES
+(1, '123 makkah St', '+962770387833', 'ayman@orange.com'),
+(2, '456 wasfi altal St', '+962770387822', 'batool@orange.com'),
+(3, '789 madenah St', '+962770387811', 'amer@orange.com'),
+(4, '321 aburashid St', '+962770387800', 'salam@orange.com'),
+(5, '654 arar St', '+962770387899', 'mohammed@orange.com');
 
-=======
-);
+INSERT INTO Coursess VALUES
+(1,'JavaScript', 1),
+(2,'C#', 2),
+(3,'Design', 3),
+(4,'MVC', 4),
+(5,'Core', 5);
 
-INSERT INTO Teachers VALUES
-(1,'Ayman', 'Jaradat'),
-(2,'Batool', 'Aldalki'),
-(3,'Amer', 'Abualhaija'),
-(4,'Salam', 'Almomani'),
-(5,'Mohammed', 'Frehat');
+INSERT INTO Studentss (FirstName, LastName) VALUES
+('Nour', 'Shloul'),
+('Tuqa', 'Harahsheh'),
+('Ahmad', 'Qadoumi'),
+('Ahmad', 'Yaqoub'),
+('Rama', 'Odat');
 
+SELECT * FROM Teacherss;
+SELECT * FROM TeacherDetailss;
+SELECT * FROM Coursess;
+SELECT * FROM Studentss;
 
-
->>>>>>> 691914f287b5a213fb8212911a0cc551d263796a
-INSERT INTO TeacherDetails VALUES
-(1,1, '123 makkah St', '+962770387833', 'ayman@orange.com'),
-(2,2, '456 wasfi altal St', '+962770387822', 'batool@orange.com'),
-(3,3, '789 madenah St', '+962770387811', 'amer@orange.com'),
-(4,4, '321 aburashid St', '+962770387800', 'salam@orange.com'),
-<<<<<<< HEAD
-(5,5, '654 arar St', '+962770387899', 'mohammed@orange.com');
-
-=======
-(5,5, '654 arar St', '+962770387899', 'mohammed@orange.com');
-
->>>>>>> 691914f287b5a213fb8212911a0cc551d263796a
-INSERT INTO Courses (CourseName, TeacherID) VALUES
-('JavaScript', 1),
-('C#', 2),
-('Design', 3),
-('MVC', 4),
-<<<<<<< HEAD
-('Core', 5);
-
-=======
-('Core', 5);
-
->>>>>>> 691914f287b5a213fb8212911a0cc551d263796a
-INSERT INTO Students (FirstName, LastName, EnrolledCourseID) VALUES
-('Nour', 'Shloul', 1),
-('Tuqa', 'Harahsheh', 2),
-('Ahmad', 'Qadoumi', 3),
-('Ahmad', 'Yaqoub', 4),
-<<<<<<< HEAD
-('Rama', 'Odat', 5);
-
-
-SELECT * FROM Teachers;
-SELECT * FROM TeacherDetails;
-SELECT * FROM Courses;
-SELECT * FROM Students;
-
-
-=======
-('Rama', 'Odat', 5);
-
-
-SELECT * FROM Teachers;
-SELECT * FROM TeacherDetails;
-SELECT * FROM Courses;
-SELECT * FROM Students;
-
-
->>>>>>> 691914f287b5a213fb8212911a0cc551d263796a
